ed00d010b109b872b4485346b1fd0eef96dc508e
wildfireone <wildfireone@gmail.com>
2015-12-22 23:21:04 +0000

    Update simple-text-scroll.py

--- simple-text-scroll.py
+++ simple-text-scroll.py
@@ -10,8 +10,8 @@ target_url = """http://pvoutput.org/service/r2/getstatus.jsp?sid=41079&key=333e8
 for line in urllib2.urlopen(target_url):
     reading = line
 reading = reading.split(",") 
-
-scrollphat.write_string(reading[2]+"   ")
+output = "{:,} Kwh".format(reading[2])
+scrollphat.write_string("   ")
 
 while True:
     scrollphat.scroll()
