cd8ef8b64a6fe2528ede9b024e5971738349f3fd
wildfireone <wildfireone@gmail.com>
2015-12-22 23:08:20 +0000

    Update solartest.py

--- solartest.py
+++ solartest.py
@@ -1,6 +1,6 @@
 ##
 import urllib2
 
-var target_url = """http://pvoutput.org/service/r2/getstatus.jsp?sid=41079&key=333e82f10971475dc09a0e88ee4a78eaf13f4a66"""
+target_url = """http://pvoutput.org/service/r2/getstatus.jsp?sid=41079&key=333e82f10971475dc09a0e88ee4a78eaf13f4a66"""
 for line in urllib2.urlopen(target_url):
     print line
